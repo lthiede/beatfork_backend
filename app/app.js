dae79d8abed71ddb2cc177a46c1f2a2140a56708
Lorenz Thiede <lorenz.07.lt@gmail.com>
2022-05-07 11:33:27 +0200

    Initial setup

---
+++ app/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+const appPort = 8000;
+const mongoHost = 'mongo';
+const mongoPort = '27017';
+const mongoURI = 'mongodb://' + mongoHost + ':' + mongoPort;
+const mongoClient = new MongoClient(mongoURI);
+
+app.use(express.json());
+
+app.get('/users', async (req, res) => {
+  const users = await req.app.locals.db.collection('users').find().toArray();
+  res.json(users);
+})
+
+app.post('/users', async (req, res) => {
+  const result = await req.app.locals.db.collection('users').insertOne(req.body);
+  if (result.acknowledged) {
+      res.send("Success");
+  } else {
+      res.status(500).send("Something went wrong");
+  }
+})
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+  
+
+mongoClient.connect(function(err, client) {
+  if (err) {
+    throw err;
+  }
+  app.locals.db = client.db('beatfork');
+  app.listen(appPort, () => {
+    console.log(`Example app listening on port ${appPort}`);
+  })
+})
